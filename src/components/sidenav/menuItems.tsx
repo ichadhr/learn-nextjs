c1d91c94d44c1fdd0b6c0850cf1e921353c5930e
ichadhr <ichadhr@gmail.com>
2024-08-12 01:43:30 +0700

    expanded submenu

--- src/components/sidenav/menuItems.tsx
+++ src/components/sidenav/menuItems.tsx
@@ -3,14 +3,20 @@
 import { ISideNav } from "@/app/types/menu";
 import { ChevronDown } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 
 const MenuItems = ({ item }: { item: ISideNav }) => {
   const { Name, Icon: IconComponent, SubMenu, Path } = item;
+  const [ Expanded, setExpanded ] = useState(false);
 
   const router = useRouter();
 
   const OnClick = () => {
+
+    if (SubMenu && SubMenu.length > 0) {
+      return setExpanded(!Expanded);
+    }
+
     router.push(Path);
   };
 
@@ -21,13 +27,18 @@ const MenuItems = ({ item }: { item: ISideNav }) => {
   }, [Path, PathName]);
 
   return (
-    <div className={`flex items-center p-3 hover:bg-sidenav-background rounded-lg cursor-pointer hover:text-sidenav-active justify-between text-sidenav-iconColor ${isActive && 'text-sidenav-active bg-sidenav-background'}`} onClick={OnClick}>
-      <div className="flex items-center space-x-2">
-        <IconComponent size={20} />
-        <p className='text-sm font-semibold'>{Name}</p>
+    <>
+      <div className={`flex items-center p-3 rounded-lg cursor-pointer justify-between text-sidenav-iconColor hover:text-sidenav-active hover:bg-sidenav-background ${isActive && 'text-sidenav-active bg-sidenav-background'}`} onClick={OnClick}>
+        <div className="flex items-center space-x-2">
+          <IconComponent size={20} />
+          <p className='text-sm font-semibold'>{Name}</p>
+        </div>
+        {SubMenu && SubMenu.length > 0 && <ChevronDown size={18} 
+        className={Expanded ? 'rotate-180 duration-200' : 'duration-200'}
+        />}
       </div>
-      {SubMenu && SubMenu.length > 0 && <ChevronDown size={18} />}
-    </div>
+      {Expanded && <p>Expanded</p>}
+    </>
   );
 };
 
